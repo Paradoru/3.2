65fc34eae2d639b0320e3c19be89b8b2f720230f
Paradoru <LicLexa@yandex.ru>
2021-11-22 01:29:58 +0200

    Добавьте файлы проекта.

---
+++ 3.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b;
+                b = 1;
+                Console.WriteLine("Вводите числа, чтобы подсчитать введите (0)");
+                while (true)
+                {
+                    a = int.Parse(Console.ReadLine());
+                    if (a == 0) break;  
+                    b *= a;
+                }
+                if (a == 0 && b == 1) Console.WriteLine("Пожалуйста, введите хотя бы одно число");
+                else
+                Console.WriteLine($"Произведение последовательности чисел равно {b}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
